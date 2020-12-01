3da10cb24a55cb740b066e89155e6ebb2d377dc1
Tony Pelle <tonypelle@gmail.com>
2020-11-30 21:56:40 -0800

    Fix indentation

--- day_1/expense_report.go
+++ day_1/expense_report.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
+    "bufio"
+    "fmt"
     "os"
     "strconv"
 )
@@ -17,24 +17,24 @@ func main() {
     fd, err := os.Open("input.txt")
     check(err)
 
-	var vals []int
+    var vals []int
 
-	scanner := bufio.NewScanner(fd)
-	for scanner.Scan() {
-		v, err := strconv.Atoi(scanner.Text())
-	    check(err)
-		vals = append(vals, v)
-	}
-	for a := 0; a < len(vals) - 2; a++ {
-		for b := a + 1; b < len(vals) - 1; b++ {
-			for c := b + 1; c < len(vals); c++ {
-				aa := vals[a]
-				bb := vals[b]
-				cc := vals[c]
-				if aa + bb + cc == 2020 {
-					fmt.Println(aa, bb, cc, aa + bb + cc, aa * bb * cc)
-				}
-			}
-		}
-	}
+    scanner := bufio.NewScanner(fd)
+    for scanner.Scan() {
+        v, err := strconv.Atoi(scanner.Text())
+        check(err)
+        vals = append(vals, v)
+    }
+    for a := 0; a < len(vals) - 2; a++ {
+        for b := a + 1; b < len(vals) - 1; b++ {
+            for c := b + 1; c < len(vals); c++ {
+                aa := vals[a]
+                bb := vals[b]
+                cc := vals[c]
+                if aa + bb + cc == 2020 {
+                    fmt.Println(aa, bb, cc, aa + bb + cc, aa * bb * cc)
+                }
+            }
+        }
+    }
 }
--- day_1/expense_report.py
+++ day_1/expense_report.py
@@ -1,9 +1,9 @@
 #!/bin/env python3
 
 if __name__ == '__main__':
-	with open('input.txt', 'r') as fp:
-		vals = [int(line.strip()) for line in fp.readlines()]
-		for i, a in enumerate(vals[:-1]):
-			for b in vals[i+1:]:
-				if a + b == 2020:
-					print(a, b, a+b, a*b)
+    with open('input.txt', 'r') as fp:
+        vals = [int(line.strip()) for line in fp.readlines()]
+        for i, a in enumerate(vals[:-1]):
+            for b in vals[i+1:]:
+                if a + b == 2020:
+                    print(a, b, a+b, a*b)
