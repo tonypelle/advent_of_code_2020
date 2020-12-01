f0866809a3759f3e87b3c7acf93fa3b8f1ccbb96
Tony Pelle <tonypelle@gmail.com>
2020-11-30 21:54:43 -0800

    Day 1, part 2

--- day_1/expense_report.go
+++ day_1/expense_report.go
@@ -25,12 +25,15 @@ func main() {
 	    check(err)
 		vals = append(vals, v)
 	}
-	for a := 0; a < len(vals) - 1; a++ {
-		for b := a + 1; b < len(vals); b++ {
-			aa := vals[a]
-			bb := vals[b]
-			if aa + bb == 2020 {
-				fmt.Println(aa, bb, aa + bb, aa * bb)
+	for a := 0; a < len(vals) - 2; a++ {
+		for b := a + 1; b < len(vals) - 1; b++ {
+			for c := b + 1; c < len(vals); c++ {
+				aa := vals[a]
+				bb := vals[b]
+				cc := vals[c]
+				if aa + bb + cc == 2020 {
+					fmt.Println(aa, bb, cc, aa + bb + cc, aa * bb * cc)
+				}
 			}
 		}
 	}
